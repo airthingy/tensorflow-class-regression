04d687a39f3f668627fa968d911aa44324fb5e64
bibhas <bibhas2@gmail.com>
2019-08-16 12:32:49 -0400

    Print prediction and actual value side by side

--- nn_predict.py
+++ nn_predict.py
@@ -5,6 +5,7 @@ import nn_model
 def predict():
     nn_regressor = nn_model.build_model()
     test_features = pd.read_csv("test_features.csv")
+    test_prices = pd.read_csv("test_price.csv")
 
     predict_input_fn = tf.estimator.inputs.pandas_input_fn(x=test_features,
                                                             y=None,
@@ -13,7 +14,11 @@ def predict():
                                                             num_epochs=1)
     results = nn_regressor.predict(input_fn = predict_input_fn)
     
-    for r in results:
-        print(r["predictions"][0])
+    for pair in zip(results, test_prices.values):
+        result = pair[0]
+        predicted_price = result["predictions"][0]
+        actual_price = pair[1][0]
+
+        print("Predicted:", predicted_price, "Actual:", actual_price)
 
 predict()
