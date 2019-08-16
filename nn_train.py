d96f7ae60c140621b0fb5034efb0637b064b5279
bibhas <bibhas2@gmail.com>
2019-08-16 15:20:15 -0400

    Format code

--- nn_train.py
+++ nn_train.py
@@ -11,11 +11,12 @@ def train_model():
     train_features = pd.read_csv("train_features.csv")
     train_prices = pd.read_csv("train_price.csv")
 
-    training_input_fn = tf.estimator.inputs.pandas_input_fn(x=train_features,
-                                                            y=train_prices["price"],
-                                                            batch_size=128,
-                                                            shuffle=True,
-                                                            num_epochs=1000)
+    training_input_fn = tf.estimator.inputs.pandas_input_fn(
+        x=train_features,
+        y=train_prices["price"],
+        batch_size=128,
+        shuffle=True,
+        num_epochs=1000)
     nn_regressor.train(input_fn = training_input_fn)
 
 train_model()
