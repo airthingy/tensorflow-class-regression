be2044c7584c28ee8735935488d3acd4ac549aa9
bibhas <bibhas2@gmail.com>
2019-08-22 02:20:56 -0400

    Fix issues with the model and sample data

--- simple_nn.py
+++ simple_nn.py
@@ -16,11 +16,13 @@ def readout_layer(input_tensor):
     return tf.add(tf.matmul(input_tensor, W), b)
 
 def build_model():
-    X = tf.placeholder(tf.float32, [None, 1])
+    n = 1 #Number of features
+    X = tf.placeholder(tf.float32, [None, n])
     Y = tf.placeholder(tf.float32, [None, 1])
 
-    layer = hidden_layer(X, 50)
-    layer = hidden_layer(X, 20)
+    layer = hidden_layer(X, 300)
+    layer = hidden_layer(X, 200)
+    layer = hidden_layer(layer, 20)
     Y_hat = readout_layer(layer)
 
     loss = tf.reduce_mean(tf.square(Y_hat - Y))
@@ -28,9 +30,10 @@ def build_model():
 
     return model, X, Y, Y_hat, loss
 
-# Sample imput data. y = 3.x + 4
-train_x = np.array([[1.0], [2.0], [3.0], [4.0]])
-train_y = train_x * 3.0 + 4.0
+# Sample imput data. y = 3.x^2 + 4
+#1000 samples from 0 to 100
+train_x = np.random.rand(1000, 1) * 100
+train_y = np.square(train_x) * 3.0 + 4.0
 
 with tf.Session() as sess:
     model, X, Y, Y_hat, loss = build_model()
@@ -50,6 +53,6 @@ with tf.Session() as sess:
 
     # Validate the model with data not used in training
     x_unseen = np.array([[6.0], [7.0], [8.0]])
-    y_expected = x_unseen * 3.0 + 4.0
+    y_expected = np.square(x_unseen) * 3.0 + 4.0
     print("Predections:", sess.run(Y_hat, {X:x_unseen}))
     print("Expected:", y_expected)
